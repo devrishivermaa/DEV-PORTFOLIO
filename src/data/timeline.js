61a4298934e461355d69cce69b0f8ac75356a70c
DEV RISHI VERMA <devrishiverma251@gmail.com>
2025-02-04 10:32:22 +0530

    updated timeline

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -1,24 +1,37 @@
 export default [
-    {
-      year: 'Aug 2024 - Dec 2024',
-      title: 'Teaching Assistant',
-      company: 'JK Lakshmipat University',
-      location: 'Jaipur, IN',
-      details: 'As a Teaching Assistant for Data Structures and Algorithms, I support sophomore students during lab hours by solving doubts, clarifying concepts, and providing hands-on coding guidance. I also mentor them on broader computer science topics and offer support. My role involves collaborating with professors to enhance course materials and ensure student success.'
-    },
-    {
-      year: 'June 2024 – Aug 2024',
-      title: 'Research Intern',
-      company: 'QWORLD',
-      location: 'Remote',
-      details: 'I focused on designing circuits for quantum algorithms using CPT symmetry in 2WQC, aiming to reduce time complexity and improve noise resilience. I presented this work at the Hypercomplex seminar in Poland, and, because of my contributions, I continue to work with them as a member of their 2WQC XPRIZE team.'
-    },
-    {
-      year: 'May 2024 - Jul 2024',
-      title: 'Summer Intern',
-      company: 'Defense Research & Development Organization (DRDO)',
-      location: 'Dehradun, IN',
-      details: 'Worked under the guidance of scientists at the Defense Electronics Application Lab (DEAL) of DRDO, improve and refine image fusion algorithms. Focused on advancing remote sensing data quality through enhanced techniques such as PAN sharpening and Principal Component Analysis (PCA).'
-    }
-  ];
-  
+  {
+    year: 'Jan 2025 – Present',
+    title: 'Research Intern',
+    company: 'Malaviya National Institute of Technology (MNIT)',
+    location: 'Jaipur, IN',
+    details: 'As a Research Intern at the Vision Analytics and Pattern Recognition (VAPR) Lab, MNIT, under Prof. Deepak Rajan Nayak, I work on medical image analysis, developing algorithms to extract meaningful insights and improve diagnostic accuracy.'
+  },
+  {
+    year: 'Jan 2025 – Present',
+    title: 'Teaching Assistant',
+    company: 'JK Lakshmipat University',
+    location: 'Jaipur, IN',
+    details: 'As a Machine Learning TA, I assist sophomores with coding assignments, clarify concepts, troubleshoot issues, and collaborate with professors to enhance course materials.'
+  },
+  {
+    year: 'Aug 2024 - Dec 2024',
+    title: 'Teaching Assistant',
+    company: 'JK Lakshmipat University',
+    location: 'Jaipur, IN',
+    details: 'As a Teaching Assistant for Data Structures and Algorithms, I support sophomore students during lab hours by solving doubts, clarifying concepts, and providing hands-on coding guidance. I also mentor them on broader computer science topics and offer support. My role involves collaborating with professors to enhance course materials and ensure student success.'
+  },
+  {
+    year: 'June 2024 – Aug 2024',
+    title: 'Research Intern',
+    company: 'QWORLD',
+    location: 'Remote',
+    details: 'I focused on designing circuits for quantum algorithms using CPT symmetry in 2WQC, aiming to reduce time complexity and improve noise resilience. I presented this work at the Hypercomplex seminar in Poland, and, because of my contributions, I continue to work with them as a member of their 2WQC XPRIZE team.'
+  },
+  {
+    year: 'May 2024 - Jul 2024',
+    title: 'Summer Intern',
+    company: 'Defense Research & Development Organization (DRDO)',
+    location: 'Dehradun, IN',
+    details: 'Worked under the guidance of scientists at the Defense Electronics Application Lab (DEAL) of DRDO, improving and refining image fusion algorithms. Focused on advancing remote sensing data quality through enhanced techniques such as PAN sharpening and Principal Component Analysis (PCA).'
+  }
+];
